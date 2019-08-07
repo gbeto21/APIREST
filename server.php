f5ebe7bbf17f6d79932e5d36c1e821961050b605
Alberto González Balderas <albertoglezb@hotmail.com>
2019-08-07 17:40:20 -0500

    Se terminaron las clases de exponer datos y de exponer un solo recurso a traves de HTTP.

---
+++ server.php
@@ -0,0 +1,64 @@
+<?php
+
+$allowedResourceTypes = [
+    'books',
+    'authors',
+    'genres',
+];
+
+$resourceType = $_GET['resource_type'];
+
+if(!in_array($resourceType, $allowedResourceTypes)){
+    die;
+}
+
+$books = [
+    1 => [
+        'titulo' => 'Lo que el viento se llevo',
+        'id_autor' => 2,
+        'id_genero' => 2,
+    ],
+    2 => [
+        'titulo' => 'La Iliada',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+    3 => [
+        'titulo' => 'La Odisea',
+        'id_autor' => 1,
+        'id_genero' => 1,
+    ],
+];
+
+//Se indica al cliente que lo que recibira es un json
+header('Content-Type: application/json');
+
+//Levantamos id del recurso buscado
+$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id']:'';
+
+//Generamos la respuesta asumiendo que el pedido es correcto
+switch (strtoupper ($_SERVER['REQUEST_METHOD']) ){
+
+    case 'GET':
+        if(empty($resourceId)){
+            echo json_encode($books);
+        }
+
+        else{
+            if(array_key_exists($resourceId, $books)){
+                echo json_encode($books[$resourceId]);
+            }
+        }
+
+    break;
+
+    case 'POST':
+    break;
+
+    case 'PUT':
+    break;
+
+    case 'DELETE':
+    break;
+
+}
