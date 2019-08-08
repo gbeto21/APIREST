039de1773716fd642aa0b67d0c6d0b9ffdd37190
Alberto González Balderas <albertoglezb@hotmail.com>
2019-08-08 11:15:43 -0500

    Se termino la clase Autenticacion HMAC

--- server.php
+++ server.php
@@ -1,9 +1,30 @@
 <?php
 
-$user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['$_PHP_AUTH_USER'] : '';
-$pwd = array_key_exists('PHP_AUTH_PW', $_SERVER) ? $_SERVER['$_PHP_AUTH_PW'] : '';
+// $user = array_key_exists('PHP_AUTH_USER', $_SERVER) ? $_SERVER['$_PHP_AUTH_USER'] : '';
+// $pwd = array_key_exists('PHP_AUTH_PW', $_SERVER) ? $_SERVER['$_PHP_AUTH_PW'] : '';
 
-if($user!== 'mauro' || $pwd !== '1234'){
+// if($user!== 'mauro' || $pwd !== '1234'){
+//     die;
+// }
+
+if(
+    !array_key_exists('HTTP_X_HASH', $_SERVER) ||
+    !array_key_exists('HTTP_X_TIMESTAMP', $_SERVER) ||
+    !array_key_exists('HTTP_X_UID', $_SERVER)
+){
+    die;
+}
+
+list($hash, $uid, $timestamp) = [
+    $_SERVER['HTTP_X_HASH'],
+    $_SERVER['HTTP_X_UID'],
+    $_SERVER['HTTP_X_TIMESTAMP'],
+];
+
+$secret = 'Sh!! No se lo cuentes a nadie!';
+$newHash = sha1($uid.$timestamp.$secret);
+
+if($newHash !== $hash){
     die;
 }
 
